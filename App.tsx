5a1f162f5f1a68b557c063bc8edf70553aa63323
Chirayu Agrawal <chirayu.a@westagilelabs.com>
2023-06-07 03:00:56 +0530

    Barebone v1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { useFonts } from "expo-font";
+import { LogBox, StyleSheet } from "react-native";
+import { Text, View } from "./components/Themed";
+import { useEffect } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [isLoaded] = useFonts({
+		SpaceMono: require("./assets/fonts/SpaceMono-Regular.ttf"),
+	});
+
+	useEffect(() => {
+		LogBox.ignoreAllLogs(true);
+		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+	}, []);
+
+	if (!isLoaded) {
+		return <View />;
+	} else {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		paddingHorizontal: "5%",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
